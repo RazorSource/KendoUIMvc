ba0f1418cea3490737270f4c2aab8ae359f4e58f
BobbySchuster <BobbySchuster@users.noreply.github.com>
2015-02-04 10:02:57 -0500

    Added additional PageOfData constructor.

--- current/KendoUIMvc/Models/PageOfData.cs
+++ current/KendoUIMvc/Models/PageOfData.cs
@@ -20,7 +20,9 @@ namespace KendoUIMvc.Models
         }
 
         /// <summary>
-        /// Constructor.
+        /// Constructor.  This constructor should be used when data is filtered down to a single
+        /// page at the datasource level.  This method will result in the best application performance,
+        /// especially when dealing with larger sets of data.
         /// </summary>
         /// <param name="data">A list of items that represent one page of data.</param>
         /// <param name="total">The total number of records across all pages.</param>
@@ -41,6 +43,35 @@ namespace KendoUIMvc.Models
             this.data = new List<TModel>();
             data.Add(model);
             this.total = 1;
+        }
+
+        /// <summary>
+        /// Constructor.  This constructor creates a page of data across the entire set of models.  This constructor
+        /// can be used if paging is desired, but paging cannot occur at the datasource level.
+        /// </summary>
+        /// <param name="allModels">All of items in the entire set of data.</param>
+        /// <param name="take">The total number of items on a page.</param>
+        /// <param name="skip">The number of records to skip.</param>
+        public PageOfData(IList<TModel> allModels, int? take, int? skip)
+        {
+            // Get the total count
+            this.total = allModels.Count;
+
+            // Apply skip filter
+            if (skip != null)
+            {
+                data = allModels.Skip(skip.Value).ToList();
+            }
+            else
+            {
+                data = allModels.ToList();
+            }
+
+            // Apply take filter
+            if (take != null)
+            {
+                data = data.Take(take.Value).ToList();
+            }
         }
 
         /// <summary>
